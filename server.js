c313207a671dde9e4c780b3d6c6281957768cb53
“yanitdm18” <mesfinyanit20@gmail.comssh-keygen -t rsa -b 4096 -C mesfinyanit20@gmail.com>
2024-02-21 01:28:28 -0500

    setting up express connection to db

--- server.js
+++ server.js
@@ -1,2 +1,31 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/taskManagerDatabase', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB database');
+});
+
+const express = require('express')
+const cors = require('cors')
+const PORT = process.env.PORT || 3001
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('This is root!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Express server listening on port ${PORT}`)
+})
+
+
 
   
