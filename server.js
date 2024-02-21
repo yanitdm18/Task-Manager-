51d5cc21554c378f7d6549c220e8121dd289ec31
“yanitdm18” <mesfinyanit20@gmail.comssh-keygen -t rsa -b 4096 -C mesfinyanit20@gmail.com>
2024-02-21 02:06:37 -0500

    defining task routes

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const cors = require('cors')
 const PORT = process.env.PORT || 3001
 
 const app = express()
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors())
 app.use(express.json())
